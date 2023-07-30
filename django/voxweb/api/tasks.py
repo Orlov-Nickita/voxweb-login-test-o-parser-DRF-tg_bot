61fac5cec00e06679047237fcfd8c2223c912665
Nikita Orlov <orlov.nickita@gmail.com>
2023-07-30 23:22:35 +0300

    docker compose was created to initialize redis, celery-worker, django, telegram bot and mysql database. Everything works. It remains to add the 'list of products'-button to the bot.

---
+++ django/voxweb/api/tasks.py
@@ -0,0 +1,47 @@
+from celery import shared_task
+import json
+from typing import List, Dict
+import os
+
+from rest_framework import status
+from rest_framework.response import Response
+
+from api.serializers import ProductSerializer
+from api.utils import get_items_from_page, download_image, send_message_in_bot
+from voxweb.loader import URL
+from voxweb.settings import MEDIA_ROOT
+
+
+@shared_task
+def parse_html(count: int):
+    try:
+        get_items_from_page(url=URL, html_file='source-page', product_count=count,
+                            json_file_name='products')
+
+        j_file = os.path.join(MEDIA_ROOT, 'products.json')
+
+        with open(j_file, 'r', encoding='utf-8') as temp:
+            products: List[Dict] = json.load(temp)
+
+        for i_pr in range(count):
+            image_url = products[i_pr].pop('image')
+            products[i_pr].update(
+                {
+                    'image': {
+                        'file': download_image(image_url),
+                        'alt': products[i_pr].get('title')
+                    }
+                }
+            )
+            ps = ProductSerializer(data=products[i_pr])
+            if ps.is_valid(raise_exception=True):
+                ps.save()
+            else:
+                return Response(ps.errors, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    except Exception as Ex:
+        err = json.dumps({'error': Ex})
+        return Response(err, status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    else:
+        send_message_in_bot(products_count=count)
